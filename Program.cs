e943d25d25b9e95b8489667c651ac4a3a955bf60
VallentinaStef <stefhanievallentina@gmail.com>
2026-03-14 12:24:27 +0700

    Manambahkan DoorMachine

--- Program.cs
+++ Program.cs
@@ -3,5 +3,15 @@ using TP_MODUL4_103022400002;
 
 class Program
 {
-    
+    static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+
+        Console.WriteLine(kodePos.getKodePos("Batununggal"));
+        Console.WriteLine(kodePos.getKodePos("Maleer"));
+
+        DoorMachine door = new DoorMachine();
+        door.BukaPintu();
+        door.KunciPintu();
+    }
 }
